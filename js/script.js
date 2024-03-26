23af381e34b31cf1362ece76c6c2321b587bb30f
leejyon55 <yeon5015@naver.com>
2024-03-26 17:45:30 +0900

    java

--- js/script.js
+++ js/script.js
@@ -13,3 +13,15 @@ $(window).on('scroll',function () {
 $(".popup>span").on("click", function () {
     $(".popup").css('display','none');
   });
+
+// 애니메이션
+gsap.to('.main-banner1',{
+    duration:3,
+    opacity:1,
+    y:-280,
+})
+// gsap.to('.section1-step1',{
+//     duration:3,
+//     opacity:1,
+//     y:-20,
+// })
