0966c31b03f652c00c0e75eccdca0b2e6799f9d9
leejyon55 <yeon5015@naver.com>
2024-03-28 10:16:53 +0900

    finish

--- js/script.js
+++ js/script.js
@@ -1,84 +1,87 @@
 const header = $('header');
-$(window).on('scroll',function () {
-    const scroll = $(window).scrollTop();
-    if(scroll <= 0 ){
-        header.removeClass('on');
-    }else{
-        header.addClass('on');
-    }
-    
-})
+$(window).on('scroll', function () {
+	const scroll = $(window).scrollTop();
+	if (scroll <= 0) {
+		header.removeClass('on');
+	} else {
+		header.addClass('on');
+	}
+});
 
 // popup
-$(".popup>span").on("click", function () {
-    $(".popup").css('display','none');
-  });
-
-
-//main banner gsap
-$(()=>{let tl = gsap.timeline({
-    repeat: -1, 
-    repeatDelay: 1 
+$('.popup>span').on('click', function () {
+	$('.popup').css('display', 'none');
 });
 
-tl.to('.main-banner1', {
-    opacity: 1,
-    duration: 1,
-    y: '-85%',
-})
-.to('.main-banner1', {
-    opacity: 0,
-    duration: 1,
-}, '+=1')
-.to('.main-banner2', {
-    opacity: 1,
-    duration: 1,
-    y: '-40%',
-}, '+=1')
-.to('.main-banner2', {
-    opacity: 0,
-    duration: 1,
-}, '+=1');})
+//main banner animation
+function animateBanners() {
+	$(".main-banner1").css({
+		top: '80%',
+		opacity: 0 
+	}).animate({
+	  opacity: 1,
+	  top: '55%'
+	}, 1500).animate({
+	  opacity: 0
+	}, 1500, function() {
+	  $(".main-banner2").css({
+		top: '80%',
+		opacity: 0 
+	  }).animate({
+		opacity: 1,
+		top: '60%'
+	  }, 1500).animate({
+		opacity: 0
+	  }, 1500, function() {
+		animateBanners(); 
+	  });
+	});
+  }
+  animateBanners(); 
+  
 
-//main banner gsap
-$(()=>{let Tl = gsap.timeline({
-    repeat: -1, 
-    repeatDelay: 1 
-});
-
-Tl.to('.main-banner3', {
-    opacity: 1,
-    duration: 1,
-    y: '-13%',
-})
-.to('.main-banner3', {
-    opacity: 0,
-    duration: 1,
-}, '+=1')
-.to('.main-banner4', {
-    opacity: 1,
-    duration: 1,
-    y: '-18%',
-}, '+=1')
-.to('.main-banner4', {
-    opacity: 0,
-    duration: 1,
-}, '+=1');})
-
+//main sm banner animation
+function animateSbanners() {
+	$(".main-banner3").css({
+		top: '300px',
+		opacity: 0 
+	}).animate({
+	  opacity: 1,
+	  top: '43%'
+	}, 1500).animate({
+	  opacity: 0
+	}, 1500, function() {
+	  $(".main-banner4").css({
+		top: '320px',
+		opacity: 0 
+	  }).animate({
+		opacity: 1,
+		top: '45%'
+	  }, 1500).animate({
+		opacity: 0
+	  }, 1500, function() {
+		animateSbanners(); 
+	  });
+	});
+  }
+  animateSbanners(); 
 
 //gsap
-gsap.to('.section1-step1',{
-    scrollTrigger:'.section1-step1',
-    y:'-100px',
-    duration: 1,
-})
-gsap.to('.section1-step2',{
-    scrollTrigger:'.section1-step2',
-    y:'-100px',
-    duration: 1,
-})
-gsap.to('.section1-step3',{
-    scrollTrigger:'.section1-step3',
-    y:'-100px',
-    duration: 1,
-})
+gsap.to('.section1-step1', {
+	scrollTrigger: '.section1-step1',
+	y: '-100px',
+	duration: 1,
+	opacity:1
+});
+gsap.to('.section1-step2', {
+	scrollTrigger: '.section1-step2',
+	y: '-100px',
+	duration: 1,
+	opacity:1
+});
+gsap.to('.section1-step3', {
+	scrollTrigger: '.section1-step3',
+	y: '-100px',
+	duration: 1,
+	opacity:1
+});
